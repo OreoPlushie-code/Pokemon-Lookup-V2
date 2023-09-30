9d0b92394da0a2bda89a829917feeab0cff2f0f1
OreoPlushie-code <142030985+OreoPlushie-code@users.noreply.github.com>
2023-09-30 14:17:54 +0400

    Add files via upload

---
+++ url_stems.py
@@ -0,0 +1,14 @@
+
+# POKEMON DATABASE URL STEMS 
+pokemon_url_stem = "https://pokemondb.net/pokedex/"
+item_url_stem = "https://pokemondb.net/item/"
+move_url_stem = "https://pokemondb.net/move/"
+abiltiy_url_stem = "https://pokemondb.net/ability/"
+
+# SMOGON URL STEMS
+smogon_pokemon_url_stem = "https://www.smogon.com/dex/sv/pokemon/"
+smogon_item_url_stem = "https://www.smogon.com/dex/sv/items/"
+smogon_moves_url_stem = "https://www.smogon.com/dex/sv/moves/"
+smogon_abilities_url_stem = "https://www.smogon.com/dex/sv/abilities/"
+
+
